0fabbb185f6e0d14dcaa63664710757d715a0599
Umer Shaikh <umershaikh305@gmail.com>
2020-05-10 03:43:27 +0500

    Add switch to handle cases of OID

--- snmp/snmp.js
+++ snmp/snmp.js
@@ -1,11 +1,25 @@
 const { SOFTWARE_VERSION, DB_QUERY, DISC_SPACE } = require('./OID_Constants.js'); // import OID constants
 
 class Snmp {
+    softwareVersionNo = '6.1.1';
 
     // This function will recieve OIDs as an argument, and then perform tasks based on these OIDs 
     trap = (oids = []) => {
         oids.map((oid, index) => {
-            console.log(SOFTWARE_VERSION)
+            switch (oid) {
+                case SOFTWARE_VERSION:
+                    console.log(`${oid} = STRING: '${this.softwareVersionNo}'`);
+                    break;
+                case DB_QUERY:
+                    console.log(oid);
+                    break;
+                case DISC_SPACE:
+                    console.log(oid);
+                    break;
+                default:
+                    console.log('invalid OID');
+                    break;
+            }
 
         })
     }
