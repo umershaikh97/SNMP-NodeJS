14ac6588453210333c49215811cdefae634d6a69
Umer Shaikh <umershaikh305@gmail.com>
2020-05-10 19:53:54 +0500

    Install PostgreSQL client and add connection string

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ const oids = [
     '1.3.6.1.2.1.1.2'       // OID which will return the total disk space used (in bytes) for the /var/log/ folder
 ];
 
+// trap function recieves OIDs as an argument, and then perform tasks based on these OIDs 
 snmp.trap(oids);
--- snmp/snmp.js
+++ snmp/snmp.js
@@ -1,17 +1,24 @@
 const { SOFTWARE_VERSION, DB_QUERY, DISC_SPACE } = require('./OID_Constants.js'); // import OID constants
 
 class Snmp {
-    softwareVersionNo = '6.1.1';
+    softwareVersionNo = '6.1.1'; // we can return the static string against the first OID (as per requirement)
 
-    // This function will recieve OIDs as an argument, and then perform tasks based on these OIDs 
+    //  username/password should never be exposed like this, they should be declared in .env (enviroment variables)
+    //  but to keep the things simple, I'm declaring it in this way. It is not recommended thou
+
+    // you need to change this username and password as per your login roles for DB
+    db_userName = 'umershaikh';
+    db_password = '12345';
+
+    // This trap function will recieve OIDs as an argument, and then perform tasks based on these OIDs 
     trap = (oids = []) => {
-        oids.map((oid, index) => {
+        oids.map(oid => {
             switch (oid) {
                 case SOFTWARE_VERSION:
                     console.log(`${oid} = STRING: '${this.softwareVersionNo}'`);
                     break;
                 case DB_QUERY:
-                    console.log(oid);
+                    const connectionString = `postgres://${this.db_userName}:${this.db_password}@localhost/afinitiTest`
                     break;
                 case DISC_SPACE:
                     console.log(oid);
